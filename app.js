6df030f5846862bace82ae53bfa7736f43d558c1
Yongji-Li <192184280+Yongji-Li@users.noreply.github.com>
2025-03-12 16:48:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('access');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello! CTI!!from b-system-combined-app');
+  res.end('Hello! CTI!!from b-system-combined-app-v2');
 });
 
 server.listen(8081, () => {
