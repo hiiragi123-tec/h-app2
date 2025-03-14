a823368ae28d0cabff27ee2dd5de3ddff95e241c
hiiragi123-tec <hirose.shuuta@cti.co.jp>
2025-03-14 17:45:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log('access');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello! CTI!!from b-system-combined-app2-logs');
+  res.end('Hello! CTI!!from b-system-combined-app2-logs_hi');
 });
 
 server.listen(8085, () => {
